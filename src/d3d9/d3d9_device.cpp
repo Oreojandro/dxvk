4ec7473d4f37be38e91221a9e3552b43270af1cf
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2024-10-05 01:13:58 +0200

    [dxvk] Remove alignment parameter from staging buffers
    Just align all suballocations to 256 bytes as usual.

--- src/d3d9/d3d9_device.cpp
+++ src/d3d9/d3d9_device.cpp
@@ -4504,7 +4504,7 @@ namespace dxvk {
     m_stagingBufferAllocated += size;
 
     D3D9BufferSlice result;
-    result.slice = m_stagingBuffer.alloc(256, size);
+    result.slice = m_stagingBuffer.alloc(size);
     result.mapPtr = result.slice.mapPtr(0);
     return result;
   }
